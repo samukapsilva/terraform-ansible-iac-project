c01700d4c18243a72159b562ab24513bd72f89de
samukapcrypto <samukapcryptoo@gmail.com>
2025-02-19 19:04:00 +0000

    separating the envitonments, configuring cicd, config files

---
+++ env/prod/ansible.sh
@@ -0,0 +1,47 @@
+cd /home/ubuntu
+curl “https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+sudo python3 get-pip.py
+sudo python3 -m pip install ansible
+tee -a playbook.yml > /dev/null <<EOT
+- hosts: localhost
+  tasks:
+    - name: Instalando o python3 e o virtualenv
+      apt:
+        pkg:
+          - python3
+          - virtualenv
+        update_cache: yes
+      become: yes
+
+    - name: Git Clone
+      ansible.builtin.git:
+        repo: https://github.com/samukapsilva/clientes-smk-api.git
+        dest: /home/ubuntu/paap
+        version: master
+        force: yes
+
+    - name: Instalando dependências com pip (Django e Django Rest)
+      pip:
+        virtualenv: /home/ubuntu/paap/venv
+        requirements: /home/ubuntu/paap/requirements.txt
+           
+      ignore_errors: yes
+
+    - name: Alterando os hosts do settings
+      lineinfile:  
+        path: /home/ubuntu/paap/setup/settings.py
+        regex: 'ALLOWED_HOSTS'
+        line: 'ALLOWED_HOSTS = ["*"]'
+        backrefs: yes
+    
+    - name: configurando o banco de dados
+      shell: '. /home/ubuntu/paap/venv/bin/activate; python /home/ubuntu/paap/manage.py migrate'
+
+    - name: carregando os dados iniciais
+      shell: '. /home/ubuntu/paap/venv/bin/activate; python /home/ubuntu/paap/manage.py loaddata clientes.json'
+
+    - name: Iniciando o Servidor
+      shell: '. /home/ubuntu/paap/venv/bin/activate; nohup python /home/ubuntu/papp/manage.py runserver 0.0.0.0:8000 &'
+EOT
+
+
--- env/prod/main.tf
+++ env/prod/main.tf
@@ -1,10 +1,14 @@
 module "aws-prod" {
-  source     = "../../infra"
-  instancia  = "t2.micro"
-  regiao_aws = "us-west-2"
-  chave      = "IaC-PROD"
+  source           = "../../infra"
+  instancia        = "t2.micro"
+  regiao_aws       = "us-west-2"
+  chave            = "IaC-PROD"
+  grupoDeSeguranca = "Producao"
+  minimo           = 1
+  maximo           = 10
+  nomeGrupo        = "Prod"
 }
 
-output "IP" {
-  value = module.aws-prod.IP_publico
-}
+#output "IP" {
+# value = module.aws-prod.IP_publico
+#}
